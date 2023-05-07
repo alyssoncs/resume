03fe6a95cc2e2f9a02bb3b8f1832d522430e1227
alysson <alysson.cirilo@gmail.com>
2023-05-06 22:02:44 -0300

    remove suspend

--- domain/infra/src/main/kotlin/alysson/cirilo/resume/infra/ResumeDriver.kt
+++ domain/infra/src/main/kotlin/alysson/cirilo/resume/infra/ResumeDriver.kt
@@ -3,5 +3,5 @@ package alysson.cirilo.resume.infra
 import alysson.cirilo.resume.entities.Resume
 
 interface ResumeDriver {
-    suspend fun convert(resume: Resume): String
+    fun convert(resume: Resume): String
 }
