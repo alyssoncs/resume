60eaf163070653492e87dcad7a1596cb1b3dfb94
alysson <alysson.cirilo@gmail.com>
2023-05-09 12:42:14 -0300

    remove old code

--- drivers/latex-sober/src/main/kotlin/alysson/cirilo/resume/drivers/LatexSoberResumeDriver.kt
+++ drivers/latex-sober/src/main/kotlin/alysson/cirilo/resume/drivers/LatexSoberResumeDriver.kt
@@ -6,7 +6,6 @@ import alysson.cirilo.resume.infra.ResumeDriver
 class LatexSoberResumeDriver : ResumeDriver {
     override fun convert(resume: Resume): String {
         val theResume = resume.reversedChronologically
-        //return javaClass.getResource("/latex-sober-resume-template.tex").readText()
         return LatexSoberResumeBuilder(theResume)
             .makeHeader()
             .makeExperiences()
