53156035fee371ec36f0480224cf145554d118c0
alysson <alysson.cirilo@gmail.com>
2023-05-11 22:22:55 -0300

    make RoleBuilder immutable

--- domain/test/data-builders/src/main/kotlin/alysson/cirilo/resume/entities/test/databuilders/RoleBuilder.kt
+++ domain/test/data-builders/src/main/kotlin/alysson/cirilo/resume/entities/test/databuilders/RoleBuilder.kt
@@ -2,30 +2,32 @@ package alysson.cirilo.resume.entities.test.databuilders
 
 import alysson.cirilo.resume.entities.Role
 
-class RoleBuilder {
-    private var title: String = "Software Engineer"
-    private var bulletPointBuilders: List<BulletPointBuilder> = listOf(BulletPointBuilder())
-    private var enrollmentPeriodBuilder: EnrollmentPeriodBuilder = period()
+class RoleBuilder private constructor(
+    private val title: String,
+    private val bulletPointBuilders: List<BulletPointBuilder>,
+    private val enrollmentPeriodBuilder: EnrollmentPeriodBuilder,
+) {
+    constructor() : this(
+        title = "Software Engineer",
+        bulletPointBuilders = listOf(BulletPointBuilder()),
+        enrollmentPeriodBuilder = period(),
+    )
 
-    fun with(bulletPointBuilders: List<BulletPointBuilder>) = builderMethod {
-        this.bulletPointBuilders = bulletPointBuilders
-    }
+    fun with(bulletPointBuilders: List<BulletPointBuilder>) =
+        RoleBuilder(title, bulletPointBuilders, enrollmentPeriodBuilder)
 
     fun with(bulletPointBuilder: BulletPointBuilder) = with(listOf(bulletPointBuilder))
 
     fun withNoBulletPoints() = with(emptyList())
 
-    fun append(bulletPointBuilder: BulletPointBuilder) = builderMethod {
-        this.bulletPointBuilders += bulletPointBuilder
-    }
+    fun append(bulletPointBuilder: BulletPointBuilder) =
+        RoleBuilder(title, bulletPointBuilders + bulletPointBuilder, enrollmentPeriodBuilder)
 
-    fun `as`(title: String) = builderMethod {
-        this.title = title
-    }
+    fun `as`(title: String) =
+        RoleBuilder(title, bulletPointBuilders, enrollmentPeriodBuilder)
 
-    fun between(enrollmentPeriodBuilder: EnrollmentPeriodBuilder) = builderMethod {
-        this.enrollmentPeriodBuilder = enrollmentPeriodBuilder
-    }
+    fun between(enrollmentPeriodBuilder: EnrollmentPeriodBuilder) =
+        RoleBuilder(title, bulletPointBuilders, enrollmentPeriodBuilder)
 
     fun build(): Role {
         return Role(
