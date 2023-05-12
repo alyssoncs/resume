15ce9adcc1a1a92a108fbba50d0bf85d819d9cc5
alysson <alysson.cirilo@gmail.com>
2023-05-11 21:58:20 -0300

    make ProjectOrPublicationBuilder immutable

--- domain/test/data-builders/src/main/kotlin/alysson/cirilo/resume/entities/test/databuilders/ProjectOrPublicationBuilder.kt
+++ domain/test/data-builders/src/main/kotlin/alysson/cirilo/resume/entities/test/databuilders/ProjectOrPublicationBuilder.kt
@@ -4,22 +4,22 @@ import alysson.cirilo.resume.entities.LinkedInformation
 import alysson.cirilo.resume.entities.ProjectOrPublication
 import java.net.URL
 
-class ProjectOrPublicationBuilder {
-    private var projectName: String = "cool project"
-    private var url: URL = URL("https://www.github.com/alyssoncs/coolproject")
-    private var description: String = "a very cool project"
+class ProjectOrPublicationBuilder private constructor(
+    private val projectName: String,
+    private val url: URL,
+    private val description: String,
+) {
+    constructor() : this(
+        projectName = "cool project",
+        url = URL("https://www.github.com/alyssoncs/coolproject"),
+        description = "a very cool project",
+    )
 
-    fun named(name: String) = builderMethod {
-        projectName = name
-    }
+    fun named(name: String) = ProjectOrPublicationBuilder(name, url, description)
 
-    fun hostedOn(urlSpec: String) = builderMethod {
-        url = URL(urlSpec)
-    }
+    fun hostedOn(urlSpec: String) = ProjectOrPublicationBuilder(projectName, URL(urlSpec), description)
 
-    fun description(description: String) = builderMethod {
-        this.description = description
-    }
+    fun description(description: String) = ProjectOrPublicationBuilder(projectName, url, description)
 
     fun build(): ProjectOrPublication {
         return ProjectOrPublication(
