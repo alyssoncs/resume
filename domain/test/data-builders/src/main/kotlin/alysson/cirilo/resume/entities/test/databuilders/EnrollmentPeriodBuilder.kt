d1ddd592c95d0387949d77415881e8ec7616fffe
alysson <alysson.cirilo@gmail.com>
2023-05-11 22:11:48 -0300

    make EnrollmentPeriodBuilder immutable

--- domain/test/data-builders/src/main/kotlin/alysson/cirilo/resume/entities/test/databuilders/EnrollmentPeriodBuilder.kt
+++ domain/test/data-builders/src/main/kotlin/alysson/cirilo/resume/entities/test/databuilders/EnrollmentPeriodBuilder.kt
@@ -3,23 +3,22 @@ package alysson.cirilo.resume.entities.test.databuilders
 import alysson.cirilo.resume.entities.EnrollmentPeriod
 import java.time.LocalDate
 
-class EnrollmentPeriodBuilder {
-    private var start: LocalDate = LocalDate.now()
-    private var end: EnrollmentPeriod.EndDate = EnrollmentPeriod.EndDate.Present
+class EnrollmentPeriodBuilder private constructor(
+    private val start: LocalDate,
+    private val end: EnrollmentPeriod.EndDate,
+) {
+    constructor() : this(
+        start = LocalDate.now(),
+        end = EnrollmentPeriod.EndDate.Present,
+    )
 
-    fun from(month: Int, year: Int) = builderMethod {
-        this.start = LocalDate.of(year, month, 1)
-    }
+    fun from(month: Int, year: Int) = EnrollmentPeriodBuilder(LocalDate.of(year, month, 1), end)
 
     fun to(month: Int, year: Int) = to(LocalDate.of(year, month, 1))
 
-    fun to(end: LocalDate) = builderMethod {
-        this.end = EnrollmentPeriod.EndDate.Past(end)
-    }
+    fun to(end: LocalDate) = EnrollmentPeriodBuilder(start, EnrollmentPeriod.EndDate.Past(end))
 
-    fun upToNow() = builderMethod {
-        this.end = EnrollmentPeriod.EndDate.Present
-    }
+    fun upToNow() = EnrollmentPeriodBuilder(start, EnrollmentPeriod.EndDate.Present)
 
     fun build(): EnrollmentPeriod {
         return EnrollmentPeriod(start, end)
