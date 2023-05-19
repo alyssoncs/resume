44d087a7fa26d28beee4868fb65ab4133557d301
alysson <alysson.cirilo@gmail.com>
2023-05-19 10:34:56 -0300

    refactor

--- drivers/shared/utils/src/main/kotlin/alysson/cirilo/resume/drivers/utils/date/DateFormatter.kt
+++ drivers/shared/utils/src/main/kotlin/alysson/cirilo/resume/drivers/utils/date/DateFormatter.kt
@@ -4,5 +4,5 @@ import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 private val locale = Locale.US
-val workDateFormatter = DateTimeFormatter.ofPattern("MMM. yyyy").withLocale(locale)
-val educationDateFormatter = DateTimeFormatter.ofPattern("yyyy").withLocale(locale)
+val workDateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMM. yyyy").withLocale(locale)
+val educationDateFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy").withLocale(locale)
