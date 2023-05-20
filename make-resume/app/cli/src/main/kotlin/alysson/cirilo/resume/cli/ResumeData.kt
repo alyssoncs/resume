63e330abc32d925fd1648e3676cff49615890616
alysson <alysson.cirilo@gmail.com>
2023-05-20 00:08:50 -0300

    fix wrong date

--- make-resume/app/cli/src/main/kotlin/alysson/cirilo/resume/cli/ResumeData.kt
+++ make-resume/app/cli/src/main/kotlin/alysson/cirilo/resume/cli/ResumeData.kt
@@ -189,7 +189,7 @@ private object Employments {
         private val androidDeveloper2 = Role(
             title = "Android Developer II",
             start = LocalDate.of(2022, 4, 1),
-            end = LocalDate.of(2021, 7, 1),
+            end = LocalDate.of(2022, 7, 1),
             BulletPoint("Implemented dependency injection model for checkout plugins to solve inefficient manual injection."),
             BulletPoint("Implemented dynamic onboarding, allowing PMs to control the order and timing of onboarding based on remote configs and plugin availability. Improving user engagement and educating users on effective checkout feature usage."),
         )
