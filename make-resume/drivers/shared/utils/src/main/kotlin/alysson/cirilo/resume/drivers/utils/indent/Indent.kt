dddf77fafc305a13508f9808d1d069d0370ea636
alysson <alysson.cirilo@gmail.com>
2024-02-18 12:00:43 -0300

    refactor

--- make-resume/drivers/shared/utils/src/main/kotlin/alysson/cirilo/resume/drivers/utils/indent/Indent.kt
+++ make-resume/drivers/shared/utils/src/main/kotlin/alysson/cirilo/resume/drivers/utils/indent/Indent.kt
@@ -1,6 +1,7 @@
 package alysson.cirilo.resume.drivers.utils.indent
 
+fun String.reindent(indentLevel: Int) = replaceIndent(baseIndent.repeat(indentLevel))
+
 private const val INDENT_WIDTH = 4
-private val baseIndent = " ".repeat(INDENT_WIDTH)
-fun String.reindent(indentLevel: Int) =
-    replaceIndent(baseIndent.repeat(indentLevel))
+private const val SPACE = " "
+private val baseIndent = SPACE.repeat(INDENT_WIDTH)
