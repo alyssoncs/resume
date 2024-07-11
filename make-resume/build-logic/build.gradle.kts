70c2101701747c7f0cc9d231ffd620ee25cfdb99
alysson <alysson.cirilo@gmail.com>
2024-07-11 13:24:13 -0300

    rename

--- make-resume/build-logic/build.gradle.kts
+++ make-resume/build-logic/build.gradle.kts
@@ -10,5 +10,5 @@ dependencies {
     implementation(libs.kotlin.gradle)
     implementation(libs.kotlin.serialization.gradle)
     implementation(libs.detekt.gradle)
-    implementation(libs.dependencies.sorter)
+    implementation(libs.dependencies.sorter.gradle)
 }
--- make-resume/build-logic/src/main/kotlin/alysson.cirilo.resume.quality.gradle.kts
+++ make-resume/build-logic/src/main/kotlin/alysson.cirilo.resume.quality.gradle.kts
@@ -1,6 +1,3 @@
-import alysson.cirilo.resume.utils.getLibrary
-import alysson.cirilo.resume.utils.versionCatalog
-
 plugins {
     id("alysson.cirilo.resume.detekt")
     id("com.squareup.sort-dependencies")
