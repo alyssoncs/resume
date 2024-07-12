c031f9a51e50424dfe4ee197cfe0e0c36b72d9b1
alysson <alysson.cirilo@gmail.com>
2024-07-12 14:25:56 -0300

    bump foojay resolver

--- make-resume/settings.gradle.kts
+++ make-resume/settings.gradle.kts
@@ -23,8 +23,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 rootProject.name = "make-resume"
