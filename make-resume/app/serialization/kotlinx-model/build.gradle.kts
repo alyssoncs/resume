4ca2d6316e5bd9864b05506fa4a4146c5c03b980
alysson <alysson.cirilo@gmail.com>
2024-08-11 11:10:13 -0300

    remove unused dependencies

--- make-resume/app/serialization/kotlinx-model/build.gradle.kts
+++ make-resume/app/serialization/kotlinx-model/build.gradle.kts
@@ -1,14 +1,8 @@
-import alysson.cirilo.resume.utils.resumeFixtures
-
 plugins {
     alias(libs.plugins.resume.kotlin.library)
     alias(libs.plugins.resume.serialization)
 }
 
 dependencies {
-    api(projects.domain.entities)
-
     implementation(libs.kotlin.serialization.json)
-
-    testImplementation(resumeFixtures(projects.domain.entities))
 }
