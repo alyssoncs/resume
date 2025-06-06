4807fbbccda3b9ade9b73847ecd67da5f8cd7ff4
alysson <alysson.cirilo@gmail.com>
2025-06-06 01:21:00 -0300

    refactor

--- make-resume/src/app/cli/src/test/kotlin/alysson/cirilo/resume/cli/AppTest.kt
+++ make-resume/src/app/cli/src/test/kotlin/alysson/cirilo/resume/cli/AppTest.kt
@@ -11,31 +11,35 @@ import java.io.PrintStream
 class AppTest {
     @Test
     fun `should parse args, generate and print resume`() {
-        val originalOut = System.out
-        val printStream = testStream()
-        System.setOut(printStream)
         val testResume = "/test-resume.yml".asResource()
 
-        main(arrayOf("-i", testResume.path, "-f", "sober"))
+        val output = capture(System.out) {
+            main(arrayOf("-i", testResume.path, "-f", "sober"))
+        }
 
-        val output = printStream.toString().trim()
         output shouldNot beEmpty()
-        System.setOut(originalOut)
     }
 
     @Test
     fun `should print to stderr on missing args`() {
-        val originalErr = System.err
-        val printStream = testStream()
-        System.setErr(printStream)
         val exitProcess = ExitProcessSpy()
 
-        app(emptyArray(), exitProcess)
+        val output = capture(System.err) {
+            app(emptyArray(), exitProcess)
+        }
 
-        val output = printStream.toString().trim()
         output shouldNot beEmpty()
         exitProcess.wasCalledWith(1) shouldBe true
-        System.setErr(originalErr)
+    }
+
+    private fun capture(stream: PrintStream, block: () -> Unit): String {
+        val testStream = testStream()
+
+        System.setOut(testStream)
+        block()
+        System.setOut(stream)
+
+        return testStream.toString().trim()
     }
 
     private fun testStream(): PrintStream {
