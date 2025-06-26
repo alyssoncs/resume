df91802bc6e34274c3aa6026d09e36fecbf32b5b
alysson <alysson.cirilo@gmail.com>
2025-06-26 08:50:29 -0300

    remove unused dependencies

--- make-resume/src/utils/resource/build.gradle.kts
+++ make-resume/src/utils/resource/build.gradle.kts
@@ -1,21 +1,4 @@
-import alysson.cirilo.resume.utils.resumeFixtures
-
 plugins {
     alias(libs.plugins.resume.kotlin.library)
     `java-test-fixtures`
-}
-
-features {
-    unitTests = true
-}
-
-dependencies {
-    api(projects.src.domain.entities)
-    api(projects.src.domain.infra)
-
-    testFixturesApi(libs.test.junit.api)
-    testFixturesImplementation(libs.test.kotest.assertions)
-    testFixturesImplementation(resumeFixtures(projects.src.domain.entities))
-
-    testImplementation(resumeFixtures(projects.src.domain.entities))
 }
